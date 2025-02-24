3395e8154c49f7d357121f3b57b92ab7269a64ea
Yash Patel <yashpatel@mednet.ucla.edu>
2025-02-24 12:58:19 -0800

    Add code for calculating Qualimap copy number

---
+++ lib/CopyNumberCalculator.groovy
@@ -0,0 +1,9 @@
+
+class CopyNumberCalculator {
+    /**
+    *   Calculate mitochondrial copy number assuming a ploidy of 2
+    */
+    public static double calculate_mitochondrial_copynumber(double autosomal_average, double mitochondrial_average, double ploidy=2) {
+        return ploidy * (mitochondrial_average / autosomal_average);
+    }
+}
---
+++ lib/CoverageHandler.groovy
@@ -0,0 +1,4 @@
+interface CoverageHandler {
+    double get_autosomal_average_coverage();
+    double get_mitochondrial_average_coverage();
+}
